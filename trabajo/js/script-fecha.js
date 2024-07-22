1e5c1f90fa2b99a36104ec429524a05698cfce8b
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-07-22 14:56:39 -0300

    add main scripts

---
+++ trabajo/js/script-fecha.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const date = document.getElementById("fecha");
+  date.innerHTML = new Date().getFullYear();
+});
---
+++ trabajo/js/script-validacion.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //   const form = document.getElementById("contactForm");
+  //   form.addEventListener("submit", function (event) {
+  //     const nombre = document.getElementById("nombre").value;
+  //     const correo = document.getElementById("correo").value;
+  //     const mensaje = document.getElementById("mensaje").value;
+  //     if (nombre === "" || correo === "" || mensaje === "") {
+  //       alert("Todos los campos son obligatorios.");
+  //       event.preventDefault();
+  //     } else if (!validateEmail(correo)) {
+  //       alert("Por favor, introduce un correo electrónico válido.");
+  //       event.preventDefault();
+  //     } else {
+  //       alert("Formulario enviado correctamente.");
+  //     }
+  //   });
+  //   function validateEmail(email) {
+  //     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  //     return re.test(email);
+  //   }
+});
